c63b3d403c966b8fbf5b0c3e3a5838172b02e105
Aurelien Duperray <duperray.2002@apside-groupe.com>
2020-04-02 10:29:08 +0200

    fix url

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -8,7 +8,7 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/Json' })
 };
 
-const baseUrl = 'http://52.169.108.3:9090/user/read';
+const baseUrl = 'http://52.169.108.3:9090/movie';
 
 @Injectable({
   providedIn: 'root'
